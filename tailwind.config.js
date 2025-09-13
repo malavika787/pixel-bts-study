c1ca9d7b75385448d8576a3a9e71f4e593bf623c
malavika787 <67189843+malavika787@users.noreply.github.com>
2025-09-13 12:55:32 +1000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"], // ✅ all your components
+  theme: {
+    extend: {
+      fontFamily: {
+        pixel: ['"Press Start 2P"', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+});
+
